81c41b08ae661788182a57b682b73ff3c49b8a2e
hiu <nguyenvanhieu13102003@gmail.com>
2025-05-14 16:35:31 +0700

    hieu delete delete unnecessary files

--- SenseLib/add_user_admin_menu.sql
+++
@@ -1,3 +0,0 @@
--- Thêm menu quản lý tài khoản vào menu admin
-INSERT INTO SenseLib.dbo.AdminMenu (MenuName, ControllerName, ActionName, ParentLevel, ItemOrder, Icon, AreaName, IdName) 
-VALUES (N'Quản lý tài khoản', 'User', 'Index', 0, 50, 'bi bi-people', 'Admin', 'user-nav'); 
